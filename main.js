4861f8327d867388897f01957a4ba2de86a9324f
David Grieser <david.grieser@galvanize.com>
2021-07-28 16:24:58 -0500

    move farm to its own file

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-class Farm {
-    constructor() {
-        this.field = ""; // or possibly 0
-        // null ?  probably shouldn't use something we don't know
-    }
-// possibly have a field with arces
-}
+import Farm from './farm.js';
 
 const myFarm = new Farm();
 // ?? see that the field is empty?
 console.log(myFarm);
+
 
 
