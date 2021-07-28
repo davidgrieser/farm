842e29b0737a08ed8c5f1765a2b2cfacf6d1aed0
David Grieser <david.grieser@galvanize.com>
2021-07-28 16:33:09 -0500

    we have a named animal Phillip

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ A farm should have a field
 The field should be empty
 
 Animals can be fed and be hungry
+Assume all new animals are hungry
 
 Pigs want more food
+We want to Name Pigs!
 
 Put the Pig in the Farm Field
--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import Farm from './farm.js';
+import Animal from './animal.js';
 
 const myFarm = new Farm();
 // ?? see that the field is empty?
+const pig = new Animal('Phillip'); // 
+
 console.log(myFarm);
+console.log(pig);
 
 
 
