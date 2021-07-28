893845d51a77987de30b7644fee7b59ef8af1461
David Grieser <david.grieser@galvanize.com>
2021-07-28 16:22:56 -0500

    now we have an empty field

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 class Farm {
+    constructor() {
+        this.field = ""; // or possibly 0
+        // null ?  probably shouldn't use something we don't know
+    }
 // possibly have a field with arces
 }
 
 const myFarm = new Farm();
+// ?? see that the field is empty?
+console.log(myFarm);
+
+
