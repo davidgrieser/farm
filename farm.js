0320e3798f6c60989892316ef14e649b133b3bda
David Grieser <david.grieser@galvanize.com>
2021-07-28 16:25:50 -0500

    actually include the new file

---
+++ farm.js
@@ -0,0 +1,9 @@
+class Farm {
+    constructor() {
+        this.field = ""; // or possibly 0
+        // null ?  probably shouldn't use something we don't know
+    }
+// possibly have a field with arces
+}
+
+export default Farm;
